8ee61f900aa5fa804ba067e4c277cda600da52a5
Student <liskotov@bk.ru>
2023-08-16 19:01:47 +0300

    Break statement

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-count = 0
-while count < 10:
-    if count == 5:
-        count += 1
-        continue
-    print(count)
-    count += 1
+count = 3
+password = '123'
+userchoice = ''
 
+while True:
+    userchoice = input('Введите пароль')
+    if password == userchoice:
+        print('Доступ разрешён')
+        break
+    count -= 1
+    if count == 0:
+        print('Попытки исчерпаны')
+        break
+    else:
+        print('Пароль не верный. Попыток осталось - ' + str(count))
+
+# str(x) - преоброзование X в строку
+# int(x) - преоброзование X в целое
+# float(x) - преоброзование X в
