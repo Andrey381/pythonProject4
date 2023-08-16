633008801a7f97c7edd4f79dc5d18f36332db97d
Student <liskotov@bk.ru>
2023-08-16 19:12:54 +0300

    Round1 by int

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-count = 3
-password = '123'
-userchoice = ''
+temperature = 39.7
+string = 'Температура - ' + str(temperature)
+string += ', это почти ' + str(int(temperature) + 1)
 
-while True:
-    userchoice = input('Введите пароль')
-    if password == userchoice:
-        print('Доступ разрешён')
-        break
-    count -= 1
-    if count == 0:
-        print('Попытки исчерпаны')
-        break
-    else:
-        print('Пароль не верный. Попыток осталось - ' + str(count))
-
-# str(x) - преоброзование X в строку
-# int(x) - преоброзование X в целое
-# float(x) - преоброзование X в Десятичную дробь
+print(string)
